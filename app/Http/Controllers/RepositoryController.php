c3dc5e4872192a8ee7c3b0873a2bc242691b2a79
Alehub369 <lievanoalejandro@gmail.com>
2023-11-13 13:33:01 -0500

    Formulario de creación

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -22,6 +22,11 @@ class RepositoryController extends Controller
 
         return view('repositories.show', compact('repository'));
     }
+
+    public function create()
+    {
+        return view('repositories.create');
+    }
     
     public function store(Request $request)
     {
