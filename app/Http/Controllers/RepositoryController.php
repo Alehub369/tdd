13b893ca2a830f9d01e0eebece3e00919c72f719
Alehub369 <lievanoalejandro@gmail.com>
2023-11-13 21:54:22 -0500

    Políticas de acceso

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -8,18 +8,16 @@ use Illuminate\Http\Request;
 
 class RepositoryController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
         return view('repositories.index', [
-            'repositories' => $request->user()->repositories
+            'repositories' => auth()->user()->repositories
         ]);
     }
 
-    public function show(Request $request, Repository $repository )
+    public function show(Repository $repository )
     {
-        if ($request->user()->id != $repository->user_id) {
-            abort(403);
-        }
+        $this->authorize('pass', $repository);
 
         return view('repositories.show', compact('repository'));
     }
@@ -36,31 +34,26 @@ class RepositoryController extends Controller
         return redirect()->route('repositories.index');
     }
 
-    public function edit(Request $request, Repository $repository )
+    public function edit(Repository $repository )
     {
-        if ($request->user()->id != $repository->user_id) {
-            abort(403);
-        }
+        $this->authorize('pass', $repository);
 
         return view('repositories.edit', compact('repository'));
     }
 
     public function update(RepositoryRequest $request, Repository $repository)
     {
-        if ($request->user()->id != $repository->user_id) {
-            abort(403);
-        }
-        
+        $this->authorize('pass', $repository);
+
         $repository->update($request->all());
 
         return redirect()->route('repositories.edit', $repository);
     }
 
-    public function destroy(Request $request, Repository $repository )
+    public function destroy(Repository $repository )
     {
-        if ($request->user()->id != $repository->user_id) {
-            abort(403);
-        }
+        $this->authorize('pass', $repository);
+
         $repository->delete();
 
         return redirect()->route('repositories.index');
